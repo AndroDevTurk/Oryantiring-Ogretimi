c50a85943709a5e062db9d0b5a2e92d7b031537d
AndroDevTurk <androdevturk@gmail.com>
2014-08-15 16:07:36 +0300

    Create Unit3.java

---
+++ Unit3.java
@@ -0,0 +1,27 @@
+package com.eka.Oryantiring_Ogretimi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AndroDevTurk
+ * Date: 05.06.2014
+ * Time: 15:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Unit2 extends Activity  {
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.unit3);
+
+
+
+        }
+
+}
